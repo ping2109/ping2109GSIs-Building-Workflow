13d435cd4a34303594d652d1a16b8dddda63adec
pingmado <75196272+ping2109@users.noreply.github.com>
2021-11-07 20:26:05 +0700

    Update out.py

--- .github/workflows/out.py
+++ .github/workflows/out.py
@@ -14,18 +14,19 @@ dab = os.getenv('SOURCEFORGE')
 bot_token = os.getenv('TOKEN')
 channel = os.getenv('CHAT_ID')
 chatid = os.getenv('TG_CHAT_IDS')
+notice = os.getenv('NOTICE')
 
 def send_test_sticker():
     try:
         telegram_notify = telegram.Bot(bot_token)
-        telegram_notify.send_sticker(chat_id=channel, sticker='CAACAgUAAxkBAAECxHdhHK8RDiVW9UPl9nXcJzen3tgVMAACrQIAAlIJ6VRjcWIcrnhvuSAE')
+        telegram_notify.send_sticker(chat_id=channel, sticker='CAACAgUAAxkBAAEDPGlhh9FuLPh1arHg7QZ-pyOJW6eI_wACEwQAAljWQFQcf8h6zv-jTyIE')
     except Exception as ex:
         print(ex)
 
 def send_test_message():
     try:
         telegram_notify = telegram.Bot(bot_token)
-        message = f"""{req}*{rom}*\nFrom *{dev}*\n\n*Information:*\n`{cat}`\n*Download:* [here]({dab})\n*File not found? Wait a bit, bot is uploading*\n\n*Credits:* [Erfan](https://github.com/Erfanoabdi) | [Velosh](https://t.me/Velosh) | [cyto](https://t.me/cytolytic) | [PruhMirror](https://t.me/PruhBruhNahMirror)\n*Built by @ping2109gsis\nJoin @ping2109gsischat*"""
+        message = f"""{req}*{rom}*\nFrom *{dev}*\n\n*Information:*\n`{cat}`\n\n*Notes from builder:*\n`{notice}`\n*Download:* [here]({dab})\n*File not found? Wait a bit, SF server is mirroring*\n\n*Credits:* [Erfan](https://github.com/Erfanoabdi) | [Velosh](https://t.me/Velosh) | [cyto](https://t.me/cytolytic) | [PruhMirror](https://t.me/PruhBruhNahMirror)\n*Built by @ping2109gsis\nJoin @ping2109gsischat*"""
 
         telegram_notify.send_message(chat_id=channel, text=message, disable_web_page_preview=True,
                                 parse_mode='Markdown')
